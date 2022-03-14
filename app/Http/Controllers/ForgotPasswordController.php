d6ae1cee5b54875a104e40823237dc65730a1b34
Vardan <vardannikolayan1@gmail.com>
2022-03-14 16:40:18 +0400

    Forgot password functionality created

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class ForgotPasswordController extends Controller
+{
+    public function index()
+    {
+        return view('forgot-password.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+        ]);
+
+        $token = Str::random(64);
+
+        DB::table('password_resets')->insert([
+            'email'      => $request->email,
+            'token'      => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        Mail::send('emails.reset-password-link', ['token' => $token], function ($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Reset Password');
+            $message->from('hello@q-software.com', 'TPUQ');
+        });
+
+        return redirect()->action([SessionsController::class, 'create'])->with('success',
+            'Please check your email address!');
+    }
+
+    public function edit($token)
+    {
+        return view('forgot-password.reset-password-form', ['token' => $token]);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'email'                 => 'required|email|exists:users',
+            'password'              => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+
+        $updatePassword = DB::table('password_resets')
+            ->where([
+                'email' => $request->email,
+                'token' => $request->token,
+            ])
+            ->first();
+
+        if (!$updatePassword) {
+            return redirect()->action([SessionsController::class, 'create'])->with('error',
+                'Please try reset your password again!');
+        }
+
+        User::where('email', $request->email)
+            ->update(['password' => Hash::make($request->password)]);
+
+        DB::table('password_resets')->where(['email' => $request->email])->delete();
+
+        return redirect()->action([SessionsController::class, 'create'])->with('success',
+            'Your password has been changed!');
+    }
+}
---
+++ resources/views/emails/reset-password-link.blade.php
@@ -0,0 +1,3 @@
+<p>Click on this <a
+            href="{{ action([\App\Http\Controllers\ForgotPasswordController::class, 'edit'], $token) }}">link</a> to
+    reset your password!</p>
